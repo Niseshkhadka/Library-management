be0de969c2a5371826147b822a0f2ebb0d06f156
rhythmstha10 <stharhythm10@gmail.com>
2026-08-12 11:09:43 +0545

    Complete accounts app: API + HTML pages, role-based dashboards, add .gitignore

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login-page')),
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('accounts.urls')),
 ]
